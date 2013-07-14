025bce37eca1f1f0e637032a18307712945590e3
Shane Mc Cormack <shanemcc@gmail.com>
2013-07-14 22:25:21 +0100

    Show local poster if one exists.

--- poster.php
+++ poster.php
@@ -1,24 +1,36 @@
 <?php
 	require_once(dirname(__FILE__) . '/functions.php');
-
-	$movie = getMovieData($_REQUEST['id']);
 	
-	if (empty($movie['poster']) || $movie['poster'] == 'N/A') {
-		$movie['poster'] = 'http://t0.gstatic.com/images?q=tbn:ANd9GcQalw3XeNDg49Z24Sy-KO5pLtfCYDnU87_kKkwnDiKWv8S2zz9IryY_SEJk';
+	$movie = getMovieData($_REQUEST['id']);
+	$remotePoster = false;
+	$poster = $movie['poster'];
+
+	if (file_exists($movie['dir'] . '/movie.tbn')) {
+		$poster = $movie['dir'] . '/movie.tbn';
+	} else {
+		$remotePoster = true;
+		if (empty($movie['poster']) || $movie['poster'] == 'N/A') {
+			$poster = 'http://t0.gstatic.com/images?q=tbn:ANd9GcQalw3XeNDg49Z24Sy-KO5pLtfCYDnU87_kKkwnDiKWv8S2zz9IryY_SEJk';
+		}
 	}
 
-	$ch = curl_init($movie['poster']);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_VERBOSE, 1);
-	curl_setopt($ch, CURLOPT_HEADER, 1);
-	$response = curl_exec($ch);
-	$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-	$header = substr($response, 0, $header_size);
-	$body = substr($response, $header_size);
+	if ($remotePoster) {
+		$ch = curl_init($poster);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		curl_setopt($ch, CURLOPT_HEADER, 1);
+		$response = curl_exec($ch);
+		$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+		$header = substr($response, 0, $header_size);
+		$body = substr($response, $header_size);
 
-	foreach (explode("\n", $header) as $header) {
-		header($header);
+		foreach (explode("\n", $header) as $header) {
+			header($header);
+		}
+
+		echo $body;
+	} else {
+		header('Content-type: image/jpeg');
+		echo file_get_contents($poster);
 	}
-
-	echo $body;
 ?>
